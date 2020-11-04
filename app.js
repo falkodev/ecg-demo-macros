a301fb624f77c0a8f31cda0883ba799ffe81fa57
Anthony Tarlao <anthony.tarlao@gmail.com>
2020-11-04 11:54:04 +0100

    first commit

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var path = require('path');
 
-var apos = require('apostrophe')({
-  shortName: 'apostrophe-boilerplate',
+module.exports = require('apostrophe')({
+  root: module,
+  shortName: 'ecg-demo-macros',
 
   // See lib/modules for basic project-level configuration of our modules
   // responsible for serving static assets, managing page templates and
@@ -13,14 +14,15 @@ var apos = require('apostrophe')({
 
     // Note: most configuration occurs in the respective
     // modules' directories. See lib/apostrophe-assets/index.js for an example.
-    
+
     // However any modules that are not present by default in Apostrophe must at
     // least have a minimal configuration here: `moduleName: {}`
 
     // If a template is not found somewhere else, serve it from the top-level
     // `views/` folder of the project
 
-    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') }
+    'apostrophe-templates': { viewsFolderFallback: path.join(__dirname, 'views') },
+    products: {}
 
   }
 });
